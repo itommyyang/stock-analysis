7dc48ca0b6d804a416ba6adabc9801e689f21af7
tommy yang <itommyyang@gmail.com>
2012-11-21 01:44:55 +0800

    remove some files

--- functions/login.php
+++
@@ -1,7 +0,0 @@
-<?php
-// this is where you create your database connection
-/*
-	mysql_connect ('localhost', 'your_db_username', 'your_db_password') or die ('access error:' . mysql_error());
-	mysql_select_db ('your_db');
-*/
-?>
